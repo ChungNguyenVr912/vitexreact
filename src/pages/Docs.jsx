2be7be314dcf8f4ae71f4038b3c3df3611768682
0100837312 <chungdeser@gmail.com>
2024-11-15 16:52:56 +0700

    check

--- src/pages/Docs.jsx
+++ src/pages/Docs.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
-import {Outlet} from "react-router-dom";
+import {Outlet, useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
-function Docs(props) {
+function Docs() {
+  const tabs = ['', 'install', 'config'];
+  const [index, setIndex] = useState(0);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate(tabs[index]);
+  }, [index])
   return (
       <>
-        <div className="text-head text-9xl font-black bg-clip-text bg-gradient-to-br from-cyan-400 via-indigo-500 to-fuchsia-200 text-transparent">Docs</div>
-        <Outlet/>
+        <div>
+          <span
+              className="text-head text-9xl font-black bg-clip-text bg-gradient-to-br from-cyan-400 via-indigo-500 to-pink-600 text-transparent select-none cursor-pointer"
+              onClick={() => setIndex(index => index < 2 ? index + 1 : 0)}
+          >
+            Docs
+          </span>
+        </div>
+        <div className="mt-2">
+          <Outlet/>
+        </div>
       </>
   );
 }
